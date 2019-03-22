0da0fd5110da4328bd21e6c1feae46e38309f8d5
ludovico.lalli <ludovico.lalli@gmail.com>
2019-03-22 09:25:49 +0100

    aggiustata grafica con cssced html

--- src/app/components/menu-component/menu-component.component.ts
+++ src/app/components/menu-component/menu-component.component.ts
@@ -13,8 +13,8 @@ export class MenuComponentComponent implements OnInit {
   menuList: MenuItem[] = [
     {id: 1, descrizione: 'home', selezionato: false, endpoint: '/' + RoutingEnum.home},
     {id: 2, descrizione: 'lista', selezionato: false, endpoint: '/'  + RoutingEnum.lista},
-    {id: 3, descrizione: 'modifica', selezionato: false, endpoint: '/' + RoutingEnum.modifica},
-    {id: 4, descrizione: 'dettaglio', selezionato: false, endpoint: '/' + RoutingEnum.dettaglio},
+    {id: 3, descrizione: 'dettaglio', selezionato: false, endpoint: '/' + RoutingEnum.dettaglio},  
+    {id: 4, descrizione: 'modifica', selezionato: false, endpoint: '/' + RoutingEnum.modifica},
   ]
 
   constructor(private router: Router) { }
